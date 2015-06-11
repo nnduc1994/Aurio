3eb41fb7f72208e43b319df0b0c0c38d9e893be7
nnduc1994 <nnduc1994@gmail.com>
2015-06-11 12:12:32 +0300

    fix the connection issues

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 from app import app
 import os
 
-#SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://aurio:admin@web412.webfaction.com/aurio"
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://nnduc_000:vip05041994@localhost/aurio"
 
 #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
